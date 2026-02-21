db834b54cf58d056177426397c756937282d29ba
Nils Witt <nils.witt@nils-witt.de>
2026-02-21 15:18:15 +0100

    Removed logging

--- src/main/java/dev/nilswitt/webmap/LoadDatabase.java
+++ src/main/java/dev/nilswitt/webmap/LoadDatabase.java
@@ -5,8 +5,6 @@ import dev.nilswitt.webmap.entities.SecurityGroup;
 import dev.nilswitt.webmap.entities.User;
 import dev.nilswitt.webmap.entities.repositories.SecurityGroupRepository;
 import dev.nilswitt.webmap.entities.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +16,6 @@ import java.util.Optional;
 @Configuration
 class LoadDatabase {
 
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
-
     @Bean
     CommandLineRunner initDatabase(UserRepository repository, SecurityGroupRepository securityGroupRepository, DatabaseInitAdminUserRecord adminUserRecord, PasswordEncoder passwordEncoder) {
         Optional<SecurityGroup> adminGroupOpt = securityGroupRepository.findByName("SuperAdmins");
@@ -28,14 +23,11 @@ class LoadDatabase {
 
         if (adminGroupOpt.isEmpty()) {
             adminGroup = new SecurityGroup("SuperAdmins", new HashSet<>(SecurityGroup.availableRoles()));
-
-            log.info("Preloading " + adminGroup);
         } else {
             adminGroup = adminGroupOpt.get();
             adminGroup.setRoles(new HashSet<>(SecurityGroup.availableRoles()));
         }
         securityGroupRepository.save(adminGroup);
-
 
         if (adminUserRecord.create().equalsIgnoreCase("true")) {
             Optional<User> adminUserOpt = repository.findByUsername(adminUserRecord.username());
