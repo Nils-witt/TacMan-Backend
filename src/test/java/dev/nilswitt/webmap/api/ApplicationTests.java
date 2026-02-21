83d8d648498387e4dac351f27726fd73e82e6e81
Nils Witt <nils.witt@nils-witt.de>
2026-02-21 15:18:33 +0100

    Added Tests

---
+++ src/test/java/dev/nilswitt/webmap/api/ApplicationTests.java
@@ -0,0 +1,17 @@
+package dev.nilswitt.webmap.api;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
+class ApplicationTests {
+
+    @Test
+    @Disabled
+    void contextLoads() {
+    }
+
+}
