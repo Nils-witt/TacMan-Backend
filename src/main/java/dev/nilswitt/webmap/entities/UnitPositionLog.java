8f144620348fe9d8d5644bd1a30a44573756306b
Nils Witt <nils.witt@nils-witt.de>
2026-03-24 00:07:53 +0100

    Improvements

--- src/main/java/dev/nilswitt/webmap/entities/UnitPositionLog.java
+++ src/main/java/dev/nilswitt/webmap/entities/UnitPositionLog.java
@@ -3,7 +3,6 @@ package dev.nilswitt.webmap.entities;
 import dev.nilswitt.webmap.api.dtos.UnitPositionLogDto;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,35 +11,43 @@ import lombok.Setter;
 @Setter
 @Getter
 public class UnitPositionLog extends AbstractEntity {
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "unit_id", nullable = false)
-    private Unit unit;
 
+  @ManyToOne(optional = false)
+  private Unit unit;
 
-    @Embedded
-    private EmbeddedPosition position;
+  @Embedded
+  private EmbeddedPosition position;
 
-    public UnitPositionLog() {
-    }
+  public UnitPositionLog() {
+  }
 
+  public UnitPositionLog(Unit unit, EmbeddedPosition position) {
+    this.unit = unit;
+    this.position = position;
+  }
 
-    public UnitPositionLog(Unit unit, EmbeddedPosition position) {
-        this.unit = unit;
-        this.position = position;
-    }
+  @Override
+  public UnitPositionLogDto toDto() {
+    return new UnitPositionLogDto(
+            this.getId(),
+            this.getCreatedAt(),
+            this.getUpdatedAt(),
+            this.unit.getId(),
+            this.position.toDto()
+    );
+  }
 
-    @Override
-    public UnitPositionLogDto toDto() {
-        return new UnitPositionLogDto(this.getId(), this.getCreatedAt(), this.getUpdatedAt(), this.unit.getId(), this.position.toDto());
-    }
-
-
-    @Override
-    public String toString() {
-        return "UnitPositionLog{" +
-                "position=" + position +
-                ", id=" + id +
-                ", unit=" + unit +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return (
+            "UnitPositionLog{" +
+                    "position=" +
+                    position +
+                    ", id=" +
+                    id +
+                    ", unit=" +
+                    unit +
+                    '}'
+    );
+  }
 }
--- src/main/java/dev/nilswitt/webmap/entities/UnitStatusUpdate.java
+++ src/main/java/dev/nilswitt/webmap/entities/UnitStatusUpdate.java
@@ -1,10 +1,8 @@
 package dev.nilswitt.webmap.entities;
 
-
 import dev.nilswitt.webmap.api.dtos.UnitStatusDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,23 +16,26 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UnitStatusUpdate extends AbstractEntity {
 
-    @Column(nullable = false)
-    private int status = 6;
+  @Column(nullable = false)
+  private int status = 6;
 
-    @Column(nullable = false)
-    private boolean acknowledged = false;
+  @Column(nullable = false)
+  private boolean acknowledged = false;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "unit_id", nullable = false)
-    private Unit unit;
+  @ManyToOne(optional = false)
+  private Unit unit;
 
-    public UnitStatusDto toDto() {
+  public UnitStatusDto toDto() {
+    return new UnitStatusDto(
+            this.id,
+            this.getCreatedAt(),
+            this.getUpdatedAt(),
+            this.status,
+            this.acknowledged,
+            this.unit.getId()
+    );
+  }
 
-        return new UnitStatusDto(this.id, this.getCreatedAt(), this.getUpdatedAt(), this.status, this.acknowledged, this.unit.getId());
-    }
-
-
-    public UnitStatusUpdate() {
-
-    }
+  public UnitStatusUpdate() {
+  }
 }
