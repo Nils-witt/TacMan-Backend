4964166fb5532c540cce527b94ce333ca146f2d9
Nils Witt <nils.witt@nils-witt.de>
2026-03-30 15:14:40 +0200

    Added ARM to dev build

--- build-dev-push.sh
+++ build-dev-push.sh
@@ -3,7 +3,7 @@ export VERSION=$(git rev-parse --short HEAD)
 mvn clean package -DskipTests
 DOCKER_TAG=$(git rev-parse --abbrev-ref HEAD)
 
-docker buildx build --platform linux/amd64 -t ghcr.io/nils-witt/tacman-backend:$DOCKER_TAG .
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/nils-witt/tacman-backend:$DOCKER_TAG .
 docker push ghcr.io/nils-witt/tacman-backend:$DOCKER_TAG
 
 #docker buildx build --platform linux/amd64 -t registry.home.nils-witt.de/tacman-backend:dev .
